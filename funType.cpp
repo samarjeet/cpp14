74cf65e5e28be39d2651cada697caf73753d8cab
Samarjeet <p.samar.j@gmail.com>
2015-12-01 22:15:18 -0500

    function auto type

--- funType.cpp
+++ funType.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 #include<typeinfo>
 
+// pipe the output to 'c++filt -t' to get the types in human-readable format
+
 using namespace std;
+
+template<typename T>
+auto fromFunction(T para) //-> decltype(para)  ** need to append this if you are working with c++11
+{
+  
+  return para;
+}
 
 auto returnFromFun(int para)  -> int 
 {
@@ -11,5 +20,8 @@ int main(){
 
   auto var = returnFromFun(1);
   cout << "Type of the function is  : " << typeid(var).name() << endl;
+
+  auto var2 = fromFunction(1);
+  cout << "Type of the function is  : " << typeid(var2).name() << endl;
   return 0;
 }
